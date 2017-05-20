669cc340654deb3acf226b906a629f5ed452a2fd
Dan S <daniel.a.snyder@yale.edu>
2017-05-20 08:17:29 -0400

    Added header description of intent of this application

---
+++ bin/prep.py
@@ -0,0 +1,9 @@
+"""
+Initially, a twitter listener to determine the magnitude and location of public
+discussion of PrEP (Truvada) on Twitter.
+
+Future work may include expansion to other social media API's and other health
+topics. This will ultimately be the backend code used on a web based graphical
+user interface to ask questions relevant to HIV researchers. Python chosen as
+the language because of the Tornado and Scikit-Learn pacakges.
+"""
