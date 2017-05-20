d6241e0c5a61ba46bde14466f061fd037a470ccb
Dan S <daniel.a.snyder@yale.edu>
2017-05-20 08:42:21 -0400

    Added some whitepace for no reason. Added user story document

--- bin/prep.py
+++ bin/prep.py
@@ -7,3 +7,4 @@ topics. This will ultimately be the backend code used on a web based graphical
 user interface to ask questions relevant to HIV researchers. Python chosen as
 the language because of the Tornado and Scikit-Learn pacakges.
 """
+
