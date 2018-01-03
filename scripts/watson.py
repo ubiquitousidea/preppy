9bc2d6f36ac39b089d99e3c9ef5961dd3095b8fe
miketynes <mtynes95@gmail.com>
2018-01-02 22:06:26 -0500

    create Watson class in watson.py

--- scripts/watson.py
+++ scripts/watson.py
@@ -13,49 +13,62 @@ import argparse
 import json
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument("infile")
-args = parser.parse_args()
-infile = args.infile
-config_file = args.config_file
-
-def get_waston_nlu(config_file):
-    """Instantiates watson NLU api object with keys from config.json"""
-    try:
+class Watson():
+    """Handles communication with IBM Watson
+    
+    Currently only talks to NLU API, 
+    Support for other watson APIs can be added should demand arise. 
+    """
+    
+    def __init__(self, config_file):
+        self.creds = self._parse_config(config_file)
+        self.nlu = self._get_nlu()
+    
+    def _parse_config(self, config_file):
         with open(config_file, "r") as f:
             config = json.load(f)
-            watson_info = config.get("watson")
-            username = watson_info.get('username')
-            password = watson_info.get('password')
-            version = watson_info.get('version')
-            nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
-                username=username,
-                password=password
-                version=version)
-        return nlu
-    except:
-        print("Unable to connect to Watson API\nCheck config.json")
-        quit()
+            creds = config.get("watson")
+            return creds
 
-def nlu_analyze(txt):
-    """Makes call to Watson NLU API"""
-    result = nlu.analyze(
-        text=txt,
-        features = [features.Concepts(), features.Keywords(), features.Emotion(), features.Sentiment()]
-        )
-    return result 
+    def _get_nlu(self):
+        """Instantiates watson NLU api object using keys from config.json"""
+        username = self.creds.get('username')
+        password = self.creds.get('password')
+        version = "2017-02-27"
+        nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
+            username=username,
+            password=password,
+            version=version)
+        return nlu
+
+    def call_nlu(self, tweet):
+        """Makes call to Watson NLU API"""
+        result = self.nlu.analyze(
+            text=tweet,
+            features = [features.Concepts(), features.Keywords(), features.Emotion(), features.Sentiment()]
+            )
+        return result 
+
+def parse_args(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tweetfile", "--tweetfile", required = True)
+    parser.add_argument("-config", "--config", required = True)
+    return parser.parse_args()
 
 def main():
-    with open(infile) as f:
+    """Procedural code remains here until it finds a home in preppy application logic"""
+    args = parse_args()
+    tweetfile = args.tweetfile 
+    config_file = args.config
+
+    with open(tweetfile) as f:
         tweets = json.load(f)
-
-        nlu = get_waston_nlu()
-
+        watson = Watson(config_file)
         for k in tweets.keys():
             try:
                 tweet_text = tweets[k]['full_text']
                 print("Analyzing tweet %s" % k)
-                tweets[k]['nlu'] = nlu_analyze(tweet_text)
+                tweets[k]['nlu'] = watson.call_nlu(tweet_text)
             except watson_developer_cloud.watson_developer_cloud_service.WatsonException:
                 print("WatsonExcepption on %s" % k)
                 tweets[k]['nlu'] = None
@@ -65,9 +78,10 @@ def main():
                 tweets[k]['nlu'] = None
                 pass
 
-        outfile = "watson_results_" + time.strftime("%Y-%m-%d_%H.%M.%S") + ".json"
-    with open("outfile", "w") as f:
-        f = json.dumps(tweets, f, indent=4)
+    outfile = "watson_results_" + time.strftime("%Y-%m-%d_%H.%M.%S") + ".json"
+    with open(outfile, "w+") as f:
+       output = json.dumps(tweets, indent=4)
+       f.write(output)
 
 if __name__ == '__main__':
     main()
